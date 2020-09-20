bb8111fdba00524de495bb602525957f6d1f4b6f
ungvert <pavel.chelyapin@gmail.com>
2020-09-20 16:22:20 +0300

    feat: Robots index list page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,33 @@
 /** @jsx jsx */
-import { Global, jsx, css } from "@emotion/core";
-import { ThemeProvider as EmotionThemeProvider } from "emotion-theming";
-import React from "react";
-import Head from "next/head";
+import { Global, jsx, css } from '@emotion/core';
+import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming';
+import React from 'react';
+import Head from 'next/head';
 import {
   StylesProvider,
   ThemeProvider as MuiThemeProvider,
-} from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import theme from "../utils/theme";
-import type { AppProps /*, AppContext */ } from "next/app";
+} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../utils/theme';
+import type { AppProps /*, AppContext */ } from 'next/app';
+
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../lib/apolloClient';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
+    const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentElement?.removeChild(jssStyles);
     }
   }, []);
 
+  console.log('pageProps', pageProps);
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
   return (
-    <React.Fragment>
+    <ApolloProvider client={apolloClient}>
       <Head>
         <title>My page</title>
         <meta
@@ -37,6 +43,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           </EmotionThemeProvider>
         </MuiThemeProvider>
       </StylesProvider>
-    </React.Fragment>
+    </ApolloProvider>
   );
 }
---
+++ src/pages/robots/index.tsx
@@ -0,0 +1,83 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import Link from 'next/link';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  Typography,
+  Link as MuiLink,
+} from '@material-ui/core';
+import { gql, useQuery } from '@apollo/client';
+
+export const ROBOTS_INDEX_QUERY = gql`
+  #   ($first: Int!, $skip: Int!) {
+  #     allPosts(orderBy: { createdAt: desc }, first: $first, skip: $skip) {
+  #       id
+  #       title
+  #       votes
+  #       url
+  #       createdAt
+  #     }
+  #     _allPostsMeta {
+  #       count
+  #     }
+  #   }
+  query robots {
+    robots(order_by: { code: asc }, limit: 10) {
+      id
+      code
+    }
+  }
+`;
+
+type RobotIndexItem = {
+  id: string;
+  code: string;
+};
+
+type RobotsIndexData = {
+  robots: RobotIndexItem[];
+};
+
+const RobotsIndexPage = () => {
+  const { loading, error, data } = useQuery<RobotsIndexData, {}>(
+    ROBOTS_INDEX_QUERY
+  );
+
+  if (error)
+    return <Typography color="error">Error loading robots: {error}</Typography>;
+  if (loading) return <Typography>Loading</Typography>;
+
+  return (
+    <Container>
+      <Typography variant="h3">Robots list</Typography>
+      <Box>
+        {data?.robots.map((robot) => {
+          return (
+            <Card key={robot.id} variant="outlined">
+              <CardContent>
+                <Typography>ID {robot.id}</Typography>
+                <Typography>Code {robot.code}</Typography>
+              </CardContent>
+
+              <CardActions>
+                <MuiLink
+                  component={Link}
+                  href={`/robots/${encodeURIComponent(robot.id)}`}
+                >
+                  Details
+                </MuiLink>
+              </CardActions>
+            </Card>
+          );
+        })}
+      </Box>
+    </Container>
+  );
+};
+
+export default RobotsIndexPage;
