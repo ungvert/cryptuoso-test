9d5e95f35835e0ec1b53cbb80809c19aefa374e2
ungvert <pavel.chelyapin@gmail.com>
2020-09-20 14:49:24 +0300

    Init with create-next-app (typescript template)

---
+++ src/pages/_app.tsx
@@ -0,0 +1,42 @@
+/** @jsx jsx */
+import { Global, jsx, css } from "@emotion/core";
+import { ThemeProvider as EmotionThemeProvider } from "emotion-theming";
+import React from "react";
+import Head from "next/head";
+import {
+  StylesProvider,
+  ThemeProvider as MuiThemeProvider,
+} from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../utils/theme";
+import type { AppProps /*, AppContext */ } from "next/app";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <StylesProvider injectFirst>
+        <MuiThemeProvider theme={theme}>
+          <EmotionThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </EmotionThemeProvider>
+        </MuiThemeProvider>
+      </StylesProvider>
+    </React.Fragment>
+  );
+}
